ad17e59d7351df298b7eee304c659f86cf0cfddb
Maciej <maciej.chajda@gmail.com>
2022-02-15 18:48:43 +0100

    random refferals

--- src/views/Cooperation.vue
+++ src/views/Cooperation.vue
@@ -52,25 +52,11 @@
 
     <div class="container d-flex">
       <Referral
-        name="Piotr Rokicki"
-        text="Polecam zdecydowanie wszystkim szukającym nowych inspiracji na
-            odmianę domowego wnętrza. Sam skorzystałem i jestem bardzo
-            zadowolony."
-        :stars="5"
-      />
-      <Referral
-        name="Piotr Rokicki"
-        text="Polecam zdecydowanie wszystkim szukającym nowych inspiracji na
-            odmianę domowego wnętrza. Sam skorzystałem i jestem bardzo
-            zadowolony."
-        :stars="5"
-      />
-      <Referral
-        name="Piotr Rokicki"
-        text="Polecam zdecydowanie wszystkim szukającym nowych inspiracji na
-            odmianę domowego wnętrza. Sam skorzystałem i jestem bardzo
-            zadowolony."
-        :stars="5"
+        v-for="refferalNumber in randomRefferals"
+        :key="referrals[refferalNumber].name"
+        :name="referrals[refferalNumber].name"
+        :text="referrals[refferalNumber].text"
+        :stars="referrals[refferalNumber].stars"
       />
     </div>
   </section>
@@ -87,12 +73,114 @@ export default {
   name: "Cooperation",
   components: { CooperationStage, Referral, LandingSection },
 
-  mounted: function () {
-    const toggleButtonIcon = document.querySelector("img#toggleMenuIcon");
-    const expandableMenu = document.getElementById("expandableMenu");
+  data() {
+    return {
+      randomRefferals: [0, 1, 2],
+      referrals: [
+        {
+          id: 1,
+          name: "Janett Moon",
+          text: "Zdecydowanie polecam! Pełen profesjonalizm począwszy od stworzenia oferty aż do odbioru końcowego. Jesteśmy zachwyceni naszym nowym wnętrzem.",
+          stars: 5,
+        },
+        {
+          id: 2,
+          name: "Piotr Rokicki",
+          text: "Polecam zdecydowanie wszystkim szukającym nowych inspiracji na odmianę domowego wnętrza. Sam skorzystałem i jestem bardzo zadowolony.",
+          stars: 5,
+        },
+        {
+          id: 3,
+          name: "Maciej Macioszek",
+          text: "Polecam zdecydowanie. Firma spełniła swoje zadanie.",
+          stars: 5,
+        },
+        {
+          id: 4,
+          name: "Kaska michalewicz",
+          text: "Budowa domu to czysta przyjemność pod okiem Fabryki Wnętrz. Wszystko wykonane wg, projektu. Wyszło pięknie.  Polecam.",
+          stars: 5,
+        },
+        {
+          id: 5,
+          name: "Marcin Golab",
+          text: "Najlepszy fachowiec w swojej dziedzinie... Pełna profesjonalna... Polecam każdemu kto marzy o wyjątkowym i niepowtarzalnym domu Panie Bartku bardzo dziękujemy",
+          stars: 5,
+        },
+        {
+          id: 6,
+          name: "Sylwia andruszkiewicz",
+          text: "Długo się zastanawiałam nad wykonaniem projektu. Zdecydowałam się i NIE żałuje. Projekt super. Polecam wszystkim Fabrykewnetrz chcącym zaoszczędzić sobie przeróbek w trakcie prac. Będę polecać wszystkim !!!",
+          stars: 5,
+        },
+        {
+          id: 7,
+          name: "marta zdunczyk",
+          text: "Gorąco polecam, otrzymałam świetny projekt.  Wszystkim będę polecać.",
+          stars: 5,
+        },
+        {
+          id: 8,
+          name: "anna gierczak",
+          text: "Bardzo polecam ! ! ! Super współpraca !  Jako jedyna firma nie bierze żadnych dodatkowych opłat za poprawki.  Projekt zrobiony dokładnie tak jak chciałam.",
+          stars: 5,
+        },
+        {
+          id: 9,
+          name: "paweł sobolewski",
+          text: "Całą współprace od pomiaru przez projektanta do samej wprowadzi do domu oceniam wzorowo.  Wszystkim polecam !",
+          stars: 5,
+        },
+        {
+          id: 10,
+          name: "Anna Jędrzejczyk",
+          text: "Zleciłam wykonanie projektu kuchni salonu i łazienki. Wyszło SUPER !!!!  Gorąco polecam.",
+          stars: 5,
+        },
+        {
+          id: 11,
+          name: "Grzegorz Kmita",
+          text: "Bardzo polecam. Nie sądziłem że remont może być przyjemny.",
+          stars: 5,
+        },
+        {
+          id: 12,
+          name: "kasia molinska",
+          text: "POLECAM !!!   Jestem bardzo zadowolona z wykonanego projektu.",
+          stars: 5,
+        },
+        {
+          id: 13,
+          name: "kasia molinska",
+          text: "POLECAM !!!   Jestem bardzo zadowolona z wykonanego projektu.",
+          stars: 5,
+        },
+      ],
+    };
+  },
 
-    expandableMenu.style.height = 0;
-    toggleButtonIcon.src = "http://localhost:8080/img/menu.723604de.svg";
+  methods: {
+    toggleMenuInit() {
+      const toggleButtonIcon = document.querySelector("img#toggleMenuIcon");
+      const expandableMenu = document.getElementById("expandableMenu");
+
+      expandableMenu.style.height = 0;
+      toggleButtonIcon.src = "http://localhost:8080/img/menu.723604de.svg";
+    },
+
+    getRandomRefferals() {
+      const min = 0;
+      const max = 12;
+
+      for (let i = 0; i < 3; i++) {
+        this.randomRefferals[i] = Math.floor(Math.random() * (max - min)) + min;
+      }
+    },
+  },
+
+  mounted: function () {
+    this.toggleMenuInit();
+    this.getRandomRefferals();
   },
 };
 </script>
