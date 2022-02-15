6116f0231c957b2f77a50eab16affa44e84fbf2f
Maciej <maciej.chajda@gmail.com>
2022-02-15 17:36:12 +0100

    partners fix

--- src/components/PartnersList/PartnersList.vue
+++ src/components/PartnersList/PartnersList.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="">
-    <a class="d-flex-wrap align-center justify-center">
+  <div class="d-flex-wrap align-center justify-center">
+    <a
+      :href="partner.url"
+      target="_blank"
+      v-for="partner in partners"
+      :key="partner.name"
+      class=""
+    >
       <img
-        v-for="partner in partners"
-        :key="partner.name"
         :src="require(`@/assets/partners/${partner.image}`)"
         alt="partner"
         class="partner-image"
@@ -20,107 +24,107 @@ export default {
         {
           image: "azzardo.webp",
           name: "Azzardo",
-          url: "",
+          url: "https://azzardo.com.pl/",
         },
         {
           image: "barlinek.webp",
           name: "Barlinek",
-          url: "",
+          url: "https://www.barlinek.com.pl/",
         },
         {
           image: "bloft.webp",
           name: "B-loft",
-          url: "",
+          url: "http://b-loft.pl/",
         },
         {
           image: "ELita.webp",
           name: "ELita",
-          url: "",
+          url: "https://elitameble.pl/",
         },
         {
           image: "elmax.pl.webp",
           name: "Elmax",
-          url: "",
+          url: "https://elmax.pl/",
         },
         {
           image: "fluffo.webp",
           name: "Fluffo",
-          url: "",
+          url: "https://fluffo.pl/pl/",
         },
         {
           image: "frezo.webp",
           name: "Frezo",
-          url: "",
+          url: "https://frezo.pl/",
         },
         {
           image: "italux.webp",
           name: "Italux",
-          url: "",
+          url: "https://www.italux.pl/pl/",
         },
         {
           image: "KERMI.webp",
           name: "Kermi",
-          url: "",
+          url: "https://www.kermi.pl/",
         },
         {
           image: "kludi.webp",
           name: "Kludi",
-          url: "",
+          url: "https://www.kludi.com/pl/",
         },
         {
           image: "kolo.webp",
           name: "Koło",
-          url: "",
+          url: "https://www.kolo.com.pl/",
         },
         {
           image: "labra.webp",
           name: "Labra",
-          url: "",
+          url: "https://labra.pl/",
         },
         {
           image: "kohlman.webp",
           name: "Kohlman",
-          url: "",
+          url: "https://kohlman.com.pl/",
         },
         {
           image: "kratkipl.webp",
           name: "Kratki.pl",
-          url: "",
+          url: "https://kratki.com/pl/",
         },
         {
           image: "logo_mirror.webp",
           name: "Mirror",
-          url: "",
+          url: "https://www.mirror.com.pl/",
         },
         {
           image: "mebin.webp",
           name: "Mebin",
-          url: "",
+          url: "https://www.mebin.pl/",
         },
         {
           image: "meble.webp",
           name: "meble.pl",
-          url: "",
+          url: "https://www.meble.pl/",
         },
         {
           image: "ORISTO.webp",
           name: "Oristo",
-          url: "",
+          url: "https://www.oristo.pl/",
         },
         {
           image: "paradyz.webp",
           name: "Paradyż",
-          url: "",
+          url: "https://www.paradyz.com/",
         },
         {
           image: "porta.webp",
           name: "Porta",
-          url: "",
+          url: "https://www.porta.com.pl/",
         },
         {
           image: "tubadzin.webp",
           name: "Tubadzin",
-          url: "",
+          url: "https://www.tubadzin.pl/",
         },
       ],
     };
